f5836015a6aae7bd43a4d3fcfd090ecd41004993
austinprice05 <ajp217@lehigh.edu>
2014-08-28 18:46:24 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,29 @@
+/////////////////////////////////////////////////////////////
+//Austin Price
+//Demo 0
+//Hello World Java Program
+//
+//  first compile the program
+//      javac HelloWorld.java
+//  run the program
+//      java HellloWorld
+//
+//  define a class
+public class HelloWorld{
+
+// add main method
+    public static void main(String[] args) {
+        
+// print statement with box around it
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2014!!  #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("test");
+        
+        //test/testtest
+    }
+}
