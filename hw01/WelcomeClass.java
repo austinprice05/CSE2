c8fdc1624412b27c2a8dca8fea7cb64e1d6522ae
austinprice05 <ajp217@lehigh.edu>
2014-08-28 22:16:47 +0000

    hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////////////////////////////
+// Austin Price
+// Started: 8/28/2014
+// Due:     9/2/2014
+// Welcome Class java program
+// 
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java Welcome Class
+//
+// define class
+public class WelcomeClass {
+    
+// add main method
+    public static void main(String[] args) {
+// Print statement with box around it
+        System.out.println("   -----------      ");
+        System.out.println("   | WELCOME |      ");
+        System.out.println("   -----------      ");
+// Print Lehigh ID with border around it       
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-A--J--P--2--1--7-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v  ");
+// Print Statement about yourself (tweet length)
+        System.out.println("I was named to the All-Rookie Team in the Patriot League for basketball and led the \nteam in Three point shots made with 52.");
+    }
+}
