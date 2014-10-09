123b16308f599aaf6952ef50f552b959862b2a96
austinprice05 <ajp217@lehigh.edu>
2014-10-09 17:28:14 +0000

    lab04

--- lab04/BigMacAgain.java
+++ lab04/BigMacAgain.java
@@ -20,13 +20,13 @@ import java.util.Scanner; // import scanner
 
 public class BigMacAgain {
     public static void main(String[] args) {
-        Scanner myScanner = new Scanner (System.in);
+        Scanner myScanner = new Scanner (System.in); // input scanner into program
         
-        double costBigMac, totalCostF,
-            costPerBigMac = 2.22,
-            costFries = 2.15;
-        int nBigMacs = 0;
-        String orderFries;
+        double costBigMac, totalCostF, // declare variable that must be computed
+            costPerBigMac = 2.22, // declare variable for cost of each big mac
+            costFries = 2.15; // declare cost of fries
+        int nBigMacs = 0; // declare/assign nBigMacs
+        String orderFries; // declare holding variable
         System.out.print("Enter the number of Big Macs (an integer > 0): "); //Enter number of big macs
          if (  myScanner.hasNextInt() ) { // checking if input integer is an integer
                 nBigMacs = myScanner.nextInt(); // if true assign input integer to variable
@@ -38,31 +38,31 @@ public class BigMacAgain {
         if (nBigMacs < 0) { // check if positive
             System.out.println("You did not enter a positive int"); //if not tell user to end program
         } // end if
-        costBigMac = nBigMacs * costPerBigMac;
-        System.out.printf("You ordered "+nBigMacs+" Big Macs or a cost of "+nBigMacs+"x2.22 = "+ "%.2f \n", costBigMac);
-        System.out.print("Do you want an order of fries (Y/y/N/n) ");
-            orderFries = myScanner.next();
-            int orderFriesLength = orderFries.length();
+        costBigMac = nBigMacs * costPerBigMac; // compute cost of big macs
+        System.out.printf("You ordered "+nBigMacs+" Big Macs or a cost of "+nBigMacs+"x2.22 = "+ "%.2f \n", costBigMac); // print number of big mac and cost to 2 decimals
+        System.out.print("Do you want an order of fries (Y/y/N/n) "); // ask user if they want fries
+            orderFries = myScanner.next(); // assign input to orderFries
+            int orderFriesLength = orderFries.length(); // find length of string
             
-            if (orderFriesLength > 1) {
-                System.out.println("Single character expected");
-                return;
-            }
-            totalCostF = costBigMac + costFries;
-            switch ( orderFries ) {
-                case "Y":
-                case "y":
-                    System.out.println("You ordered fries at a cost of $2.15");
-                    System.out.printf("The total cost of the meal is "+ "%.2f \n", totalCostF);
-                    break;
-                case "N":
-                case "n":
-                    System.out.printf("The total cost of the meal is "+ "%.2f \n", costBigMac);
-                    break;
-                default:
-                    System.out.println("You did not enter one of 'y', 'Y', 'n', or 'N'");
-                    break;
-            }
+            if (orderFriesLength > 1) { // if string is greater than 1
+                System.out.println("Single character expected"); // tell user string is too long
+                return; // leave program
+            } // end if
+            totalCostF = costBigMac + costFries; // compute total cost plus fries
+            switch ( orderFries ) { // start switch statement for orderFries
+                case "Y": // case Y drop to case y
+                case "y": // if the case is y
+                    System.out.println("You ordered fries at a cost of $2.15"); // tell user cost of fries they ordered
+                    System.out.printf("The total cost of the meal is "+ "%.2f \n", totalCostF); // print cost + fries
+                    break; // stop switch
+                case "N": // case N drop to case n
+                case "n": // if the case is n
+                    System.out.printf("The total cost of the meal is "+ "%.2f \n", costBigMac); // print cost without fries
+                    break; // stop switch
+                default: // if not y or n
+                    System.out.println("You did not enter one of 'y', 'Y', 'n', or 'N'"); // tell user invalid input
+                    break; // stop switch
+            } // end switch
         
     } // end method
 } // end class
