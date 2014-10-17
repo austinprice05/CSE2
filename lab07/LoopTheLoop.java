8df41ea73676841c354fd7c25ebc2fcff863856d
austinprice05 <ajp217@lehigh.edu>
2014-10-17 15:51:40 +0000

    lab07 updated

--- lab07/LoopTheLoop.java
+++ lab07/LoopTheLoop.java
@@ -30,33 +30,43 @@ public class LoopTheLoop {
         Scanner myScanner = new Scanner (System.in); // input scanner into program
         int nStars = 0; // declare nStars and store value
         int counter = 1; // declare counter and store value
-        int counter2 = 0;
-        int counter3=0;// declare counter2 and store value
+        int counter2 = 0; // declare counter2 and store value
+        int counter3=0;// declare counter3 and store value
         
+    while (true) { //infinite while loop if user wants to run code forever
         while ( nStars > 15 || nStars < 1 ) { // when nStars is outside of range prompt user to enter input
-            System.out.print("Enter an int between 1 and 15: ");
-                while(!myScanner.hasNextInt()){
+            System.out.print("Enter an int between 1 and 15: "); // ask user to input number within range
+                while(!myScanner.hasNextInt()){ // when input is not an int
     	            myScanner.next(); //get rid of the junk entered by user
-    	            System.out.print("You did not enter an int; try again- ");
-                }
-                nStars = myScanner.nextInt();
-        }
+    	            System.out.print("You did not enter an int; try again- "); // prompt the user to an input within range again and if user input int not in range remind user to print with in range with original while loop
+                } // end while
+                nStars = myScanner.nextInt(); // set input value to nStars
+        } // end while
 
-            while (counter <= nStars) {
-                System.out.print("*");
-                counter++;
-            }
+            while (counter <= nStars) { // while counter is less than nStars
+                System.out.print("*"); // print out a star
+                counter++; // add 1 to counter
+            } // end while
             
-            while (counter2 < nStars) {
-                System.out.print("\n");
-                counter2++;
-                while (counter3 < counter2) {
-                System.out.print("*");
-                counter3++;
+            while (counter2 < nStars) { // while counter 2 is less than nStars
+                System.out.print("\n"); // print new line
+                counter2++; // add one to counter2
+                while (counter3 < counter2) { // while counter3 is less than counter2
+                System.out.print("*"); // print a star
+                counter3++; // add one to counter3
+                }// nested end while
+                counter3 = 0;// reset counter3
+            } // end while
+            System.out.print("\n"); // print new line
+            nStars = 0; // reset nStars
+            counter = 0; // reset counter lines (62-65)
+            counter2 = 0;
+            counter3 = 0;
+            System.out.print("Enter y or Y to go again: ");
+                String goAgain = myScanner.next().toLowerCase();
+                if ( !goAgain.equals("y") ) {
+                    break;
                 }
-                counter3 = 0;
-            }
-            System.out.print("\n");
-         // end of outside while
+    } //infinite while loop end
     } // end of method
 } // end of class
